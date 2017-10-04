1878e4d96977a9bf4f206acad95b6491a8e77a6f
Jake Perez <jacobanthony.perez@gmail.com>
2017-10-04 14:45:05 -0700

    complete functional-shared-pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,39 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {
+    place: 0,
+    storage: {},
+
+  }
+
+  function extend(obj1, obj2) {
+    for ( var key in obj2 ) {
+      if ( !obj1[key] ) {
+        obj1[key] = obj2[key];
+      }
+    }
+  }
+
+  extend(stack, stackMethods);
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
-
+  push: function (val) {
+    this.storage[this.place] = val;
+    this.place++;
+  },
+  size: function () {
+    return this.place;
+  },
+  pop: function () {
+    if ( this.place > 0 ) {
+      this.place--;
+      var temp = this.storage[this.place];
+      delete this.storage[this.place];
+      return temp;
+    }
+  }
+};
