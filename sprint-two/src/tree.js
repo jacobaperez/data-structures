3a4d8bee81487dd52a09dbc760d8be3c7b30bdf2
Jake Perez <jacobanthony.perez@gmail.com>
2017-10-05 16:13:16 -0700

    complete tree and add tests to treeSpec

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,7 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,13 +9,43 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  let tree = new Tree(value);
+  this.children.push(tree);
+
 };
 
 treeMethods.contains = function(target) {
+
+  var results = [];
+
+  function search(target, node) {
+    if ( node.value === target ) {
+      results.push(true);
+    }
+    if ( node.value !== target && node.children.length === 0) {
+     results.push(false);
+    } else {
+      for ( var i = 0; i < node.children.length; i++) {
+        search(target, node.children[i]);
+      }
+    }
+  }
+
+  search(target, this);
+
+  for (var i = 0; i < results.length; i++) {
+    if ( results[i] ) {
+      return true;
+    }
+  }
+  return false;
 };
+
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+    addChild - O(1);
+    contains - O(n) although I think mine may be O(n^2);
  */
