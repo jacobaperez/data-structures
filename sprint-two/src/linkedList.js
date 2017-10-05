bccbf267a0a883c9ee72f5f7aface5b7540bb25f
Jake Perez <jacobanthony.perez@gmail.com>
2017-10-05 10:23:03 -0700

    complete linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,46 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    let node = new Node(value);
+    if ( this.tail === null ) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      var current = this.head;
+      while ( current.next !== null ) {
+        current = current.next;
+      }
+      current.next = node;
+      this.tail = current.next;
+    }
   };
 
   list.removeHead = function() {
+    if ( this.head !== null ) {
+      var current = this.head;
+      var tempValue = this.head.value;
+      this.head = current.next;
+      return tempValue;
+    }
   };
 
   list.contains = function(target) {
+    if ( this.head !== null ) {
+      var current = this.head;
+
+      var inner = function(target, state) {
+        if ( state.value === target ) {
+          return true;
+        }
+        if ( state.next === null ) {
+          return false;
+        }
+        return inner(target, state.next);
+      }
+
+      return inner(target, current);
+    }
+    return false;
   };
 
   return list;
