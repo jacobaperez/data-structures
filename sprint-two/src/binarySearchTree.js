e820ef89af6899437065bb02c828a2e042d8f30c
Jake Perez <jacobanthony.perez@gmail.com>
2017-10-06 15:44:42 -0700

    binary insert method complete

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,48 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(bstMethods);
+
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+
+  return tree;
 };
 
+var bstMethods = {};
 
+bstMethods.insert = function (value) {
+  var current = this;
+
+  var inner = function (value) {
+    if ( value <= current.value ) {
+      if ( current.left === null ) {
+        return current.left = new BinarySearchTree(value);
+      }
+      current = current.left;
+      inner(value);
+    }
+    if ( current.right === null ) {
+      return current.right = new BinarySearchTree(value);
+    }
+    current = current.right;
+    inner(value);
+  }
+  inner(value);
+
+};
+
+bstMethods.contains = function (value) {
+  // var current = this;
+  // if ( current.head === value ) {
+  //   return true;
+  // }
+  
+
+};
+
+bstMethods.depthFirstLog = function () {
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
